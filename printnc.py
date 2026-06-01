4b49035b75add450084d813e8f5d1900c6f255e0
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-06-01 14:05:40 -0700

    feat: write printnc.py to view *.nc metadata

---
+++ printnc.py
@@ -0,0 +1,18 @@
+"""
+printnc.py is a utility program that prints *.nc metadata to sys.stdout.
+
+Usage: python printnc.py <file>
+
+Written by William Chuter-Davies
+"""
+
+# Standard Imports
+import sys
+
+# Third Party Imports
+from netCDF4 import Dataset
+
+filename = sys.argv[1]
+dataset  = Dataset(filename)
+
+print(dataset)
