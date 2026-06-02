af5217c0423da00ea7a2c0c7940d4fb98e10d74d
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-06-02 11:09:16 -0700

    feat: add __main__.py

---
+++ src/__main__.py
@@ -0,0 +1,27 @@
+"""
+__main__.py
+
+The purpose of this program is to compute, for each of the variables
+grouped under the Lakes ECV, a dataset comprising monthly mean values
+spanning September 1992 - December 2023, wherein each record corresponds
+to a single lake within the candidate set. For more information, refer
+to section 1.0 of the README.
+
+Usage: python __main__.py <nc_path> <shp_path>
+
+Example:
+
+```sh
+```
+
+Written by William Chuter-Davies
+"""
+
+# Standard Library Imports
+import sys
+
+def main() -> int:
+	return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
