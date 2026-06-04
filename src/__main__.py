e308686a53f385fc29195d6287320625df9dbe20
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-06-04 16:35:19 -0700

    chore: slate __main__.py

--- src/__main__.py
+++ src/__main__.py
@@ -1,27 +1,25 @@
-"""
+r"""
 __main__.py
 
-The purpose of this program is to compute, for each of the variables
-grouped under the Lakes ECV, a dataset comprising monthly mean values
-spanning September 1992 - December 2023, wherein each record corresponds
-to a single lake within the candidate set. For more information, refer
-to section 1.0 of the README.
+Description: 
 
-Usage: python __main__.py <nc_path> <shp_path>
+Usage: 
 
 Example:
-
-```sh
-```
+   ```sh
+   ```
 
 Written by William Chuter-Davies
 """
 
+
 # Standard Library Imports
 import sys
 
+
 def main() -> int:
 	return 0
+
 
 if __name__ == '__main__':
     sys.exit(main())
