217c6f82f6537113076d9c70dfcb9831edca63e2
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-06-05 11:03:50 -0700

    tests: adjust test_get_geometry_mask_returns_expected_ndarray_if_geometry_completely_overlaps_raster and test_get_geometry_mask_returns_expected_ndarray_if_geometry_partially_overlaps_raster tests and write test_get_geometry_mask_raises_value_error_if_resolution_is_zero

--- tests/test_get_geometry_mask.py
+++ tests/test_get_geometry_mask.py
@@ -2,10 +2,7 @@ r"""
 test_get_geometry_mask.py
 
 Description: 
-    The purpose of this program is to pre-compute and save, for each
-    feature in the attribute table of a Shapefile, a geometry mask for
-    application against netCDF files of a common coordinate reference
-    system (CRS) and spatial resolution.
+   The purpose of this file is to test lib.utils.get_geometry_mask.
 
 Written by William Chuter-Davies
 """
@@ -15,59 +12,69 @@ import fractions
 
 # Related Third-party Imports
 import pytest
+import numpy
 import shapely
 
 from lib.utils import get_geometry_mask
 
 
-def test_get_geometry_mask_returns_ndarray_of_dtype_bool(sample_geometry, sample_resolution):
-    geometry_mask, _ = get_geometry_mask(sample_geometry, sample_resolution)
+def test_get_geometry_mask_returns_ndarray_of_dtype_bool(sample_geometry, 
+                                                         sample_resolution) -> None:
+    geometry_mask, _ = get_geometry_mask(sample_geometry, 
+                                         sample_resolution)
 
     assert geometry_mask.dtype == bool
 
 
-def test_get_geometry_mask_returns_shape_1x1_if_geometry_is_unit_square_and_resolution_is_1(sample_geometry: shapely.geometry.base.BaseGeometry):
-    geometry_mask, _ = get_geometry_mask(sample_geometry, fractions.Fraction(1, 1))
+def test_get_geometry_mask_returns_shape_1x1_if_geometry_is_unit_square_and_resolution_is_1(sample_geometry: shapely.geometry.base.BaseGeometry) -> None:
+    geometry_mask, _ = get_geometry_mask(sample_geometry, 
+                                         fractions.Fraction(1, 
+                                                            1))
 
     assert geometry_mask.shape == (1, 1)
 
 
-def test_get_geometry_mask_returns_shape_2x2_if_geometry_is_unit_square_and_resolution_is_one_half(sample_geometry: shapely.geometry.base.BaseGeometry):
-    geometry_mask, _ = get_geometry_mask(sample_geometry, fractions.Fraction(1, 2))
+def test_get_geometry_mask_returns_shape_2x2_if_geometry_is_unit_square_and_resolution_is_one_half(sample_geometry: shapely.geometry.base.BaseGeometry) -> None:
+    geometry_mask, _ = get_geometry_mask(sample_geometry, fractions.Fraction(1, 
+                                                                             2))
 
     assert geometry_mask.shape == (2, 2)
 
 
-def test_get_geometry_mask_returns_shape_4x4_if_geometry_is_unit_square_and_resolution_is_one_fourth(sample_geometry: shapely.geometry.base.BaseGeometry):
-    geometry_mask, _ = get_geometry_mask(sample_geometry, fractions.Fraction(1, 4))
+def test_get_geometry_mask_returns_shape_4x4_if_geometry_is_unit_square_and_resolution_is_one_fourth(sample_geometry: shapely.geometry.base.BaseGeometry) -> None:
+    geometry_mask, _ = get_geometry_mask(sample_geometry, fractions.Fraction(1, 
+                                                                             4))
 
     assert geometry_mask.shape == (4, 4)
 
 
-def test_get_geometry_mask_returns_expected_ndarray_if_geometry_completely_overlaps_raster(sample_resolution: fractions.Fraction):
+def test_get_geometry_mask_returns_expected_ndarray_if_geometry_completely_overlaps_raster(sample_resolution: fractions.Fraction) -> None:
     geometry_mask, _ = get_geometry_mask(shapely.Polygon([(0, 0), 
                                                           (0, 1), 
                                                           (1, 1),
                                                           (1, 0)]),
                                          sample_resolution)
 
-    assert geometry_mask.shape == (2, 2)
-    assert geometry_mask.all()
+    numpy.testing.assert_array_equal(geometry_mask, 
+                                     numpy.array([(True, True), 
+                                                  (True, True)]))
 
 
-def test_get_geometry_mask_returns_expected_ndarray_if_geometry_partially_overlaps_raster(sample_resolution: fractions.Fraction):
+def test_get_geometry_mask_returns_expected_ndarray_if_geometry_partially_overlaps_raster(sample_resolution: fractions.Fraction) -> None:
     geometry_mask, _ = get_geometry_mask(shapely.Polygon([(0, 0), 
                                                           (0, 1), 
                                                           (1, 1)]),
                                         sample_resolution)
 
-    assert geometry_mask.shape == (2, 2)
-    assert geometry_mask.any()
-    assert not geometry_mask.all()
+    numpy.testing.assert_array_equal(geometry_mask, 
+                                     numpy.array([(True, True), 
+                                                  (True, False)]))
 
 
-def test_get_geometry_mask_returns_correct_transform_if_geometry_is_unit_square_and_resolution_is_one_half(sample_geometry: shapely.geometry.base.BaseGeometry, sample_resolution: fractions.Fraction):
-    _, transform = get_geometry_mask(sample_geometry, sample_resolution)
+def test_get_geometry_mask_returns_correct_transform_if_geometry_is_unit_square_and_resolution_is_one_half(sample_geometry: shapely.geometry.base.BaseGeometry, 
+                                                                                                           sample_resolution: fractions.Fraction) -> None:
+    _, transform = get_geometry_mask(sample_geometry, 
+                                     sample_resolution)
 
     assert transform.a ==  float(sample_resolution)
     assert transform.e == -float(sample_resolution)
@@ -75,19 +82,27 @@ def test_get_geometry_mask_returns_correct_transform_if_geometry_is_unit_square_
     assert transform.f == sample_geometry.bounds[3]
 
 
-def test_get_geometry_mask_raises_value_error_if_geometry_is_empty(sample_resolution):
+def test_get_geometry_mask_raises_value_error_if_geometry_is_empty(sample_resolution) -> None:
     with pytest.raises(ValueError):
-        _, _ = get_geometry_mask(shapely.Polygon(), sample_resolution)
+        _, _ = get_geometry_mask(shapely.Polygon(), 
+                                 sample_resolution)
 
 
-def test_get_geometry_mask_raises_value_error_if_geometry_produces_zero_width_or_zero_height_raster(sample_resolution):
+def test_get_geometry_mask_raises_value_error_if_resolution_is_zero(sample_geometry) -> None:
+    with pytest.raises(ValueError):
+        _, _ = get_geometry_mask(sample_geometry, 
+                                 fractions.Fraction(0, 
+                                                    1))
+
+
+def test_get_geometry_mask_raises_value_error_if_geometry_produces_zero_width_or_zero_height_raster(sample_resolution) -> None:
     with pytest.raises(ValueError):
         _, _ = get_geometry_mask(shapely.Polygon([(0, 0), 
                                                   (0, 1)]), 
                                  sample_resolution)
 
 
-def test_get_geometry_mask_raises_value_error_if_resolution_produces_zero_width_or_zero_height_raster(sample_geometry):
+def test_get_geometry_mask_raises_value_error_if_resolution_produces_zero_width_or_zero_height_raster(sample_geometry) -> None:
     with pytest.raises(ValueError):
         _, _ = get_geometry_mask(sample_geometry, 
                                  fractions.Fraction(-1, 2))
