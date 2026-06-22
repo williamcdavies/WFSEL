b4416b992c68066abf392edd15aa79de2f252626
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-06-22 13:31:21 -0700

    chore: rename get_and_save_geometry_masks.py to get_and_write_geometry_masks.py

--- src/lib/get_and_write_geometry_masks.py
+++ src/lib/get_and_write_geometry_masks.py
@@ -1,17 +1,18 @@
 r"""
-get_and_save_geometry_masks.py
+get_and_write_geometry_masks.py
 
 Description: 
-    The purpose of this program is to pre-compute and save, for each
+    The purpose of this program is to pre-compute and write, for each
     feature in the attribute table of a Shapefile, a geometry mask for
     application against netCDF files of a common coordinate reference
     system (CRS) and spatial resolution.
 
 Usage: 
-    python get_and_save_geometry_masks.py <shp_path>
+    python get_and_write_geometry_masks.py <src_path> <dst_path>
 
 Example:
     ```sh
+    python get_and_write_geometry_masks.py ~/Downloads/WFSEL/ESA/lakes_cci_v2.1.0_shp/lakescci_v2.1.0_data-availability.shp ~/Downloads/WFSEL/ESA/masks
     ```
 
 Written by William Chuter-Davies
@@ -27,7 +28,7 @@ import sys
 import geopandas
 
 # Local Application/Library Specific Imports
-from lib.utils import get_geometry_mask
+from lib.utils import get_geometry_mask, write_geometry_mask
 
 
 # Global Definitions
@@ -36,25 +37,32 @@ RETURN_FAILURE = 1
 
 
 def main() -> int:
-    # If argument count is not equal to 2, return with `RETURN_FAILURE`
-    if len(sys.argv) != 2:
+    # If argument count is not equal to 3, return with `RETURN_FAILURE`
+    if len(sys.argv) != 3:
         print(f'fatal: unexpected argument count: {sys.argv}')
         
         return RETURN_FAILURE
 
-    # Read `sys.argv[1]` into `shp_path`
-    shp_path = pathlib.Path(sys.argv[1])
+    # Read `sys.argv[1]` into `src_path`
+    src_path = pathlib.Path(sys.argv[1])
+    dst_path = pathlib.Path(sys.argv[2])
     
-    # If `shp_path` does not exist, return with `RETURN_FAILURE`
-    if not shp_path.exists():
+    # If `src_path` does not exist, return with `RETURN_FAILURE`
+    if not src_path.exists():
         print(f'fatal: no such file or directory: {sys.argv[1]}')
+
+        return RETURN_FAILURE
+    
+    # If `dst_path` does not exist, return with `RETURN_FAILURE`
+    if not dst_path.exists():
+        print(f'fatal: no such file or directory: {sys.argv[2]}')
 
         return RETURN_FAILURE
 
     # Attempt to ...
     try:
         # ... open GeoDataFrame specified by `shp_path`
-        gdf = geopandas.read_file(shp_path)
+        gdf = geopandas.read_file(src_path)
     # On exception, return with `RETURN_FAILURE`
     except Exception as e:
         print(f'fatal: exception: {e}')
@@ -76,10 +84,17 @@ def main() -> int:
 
         return RETURN_FAILURE
 
-    for index, data in gdf.iterrows():
+    # For each row in gdf ...
+    for _, data in gdf.iterrows():
+        # ... compute geometry_mask and transform
         geometry_mask, transform = get_geometry_mask(data[active_geometry_name], 
                                                      fractions.Fraction(1, 
                                                                         120))
+        
+        # ... write geometry_mask and transform
+        write_geometry_mask(geometry_mask, 
+                            transform, 
+                            dst_path / f'{data['id']}.TIFF')
     
     return RETURN_SUCCESS
 
--- tests/test_write_geometry_mask.py
+++ tests/test_write_geometry_mask.py
@@ -22,27 +22,38 @@ from lib.utils import get_geometry_mask, write_geometry_mask
 
 def test_write_geometry_mask_output_geometry_mask_matches_input_geometry_mask(sample_geometry, 
                                                                               sample_resolution) -> None:
-   geometry_mask, transform = get_geometry_mask(sample_geometry, sample_resolution)
+   geometry_mask, transform = get_geometry_mask(sample_geometry, 
+                                                sample_resolution)
    tmp_dir                  = os.environ['TMPDIR']
    now_str                  = datetime.datetime.now().strftime(f'%y%j%H%M%S%f')
-   path                     = pathlib.Path(tmp_dir + now_str + '.TIFF')
+   path                     = pathlib.Path(tmp_dir 
+                                           + now_str 
+                                           + '.TIFF')
    
-   write_geometry_mask(geometry_mask, transform, path)
+   write_geometry_mask(geometry_mask, 
+                       transform, 
+                       path)
 
    with rasterio.open(path) as ds:
       actual = ds.read(1)
 
-   numpy.testing.assert_array_equal(actual, geometry_mask)
+   numpy.testing.assert_array_equal(actual, 
+                                    geometry_mask)
 
 
 def test_write_geometry_mask_output_transform_matches_input_transform(sample_geometry, 
                                                                       sample_resolution) -> None:
-   geometry_mask, transform = get_geometry_mask(sample_geometry, sample_resolution)
+   geometry_mask, transform = get_geometry_mask(sample_geometry, 
+                                                sample_resolution)
    tmp_dir                  = os.environ['TMPDIR']
    now_str                  = datetime.datetime.now().strftime(f'%y%j%H%M%S%f')
-   path                     = pathlib.Path(tmp_dir + now_str + '.TIFF')
+   path                     = pathlib.Path(tmp_dir 
+                                           + now_str 
+                                           + '.TIFF')
    
-   write_geometry_mask(geometry_mask, transform, path)
+   write_geometry_mask(geometry_mask, 
+                       transform, 
+                       path)
 
    with rasterio.open(path) as ds:
       actual = ds.transform
