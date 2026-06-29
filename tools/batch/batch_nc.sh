d6682a140b48a702af308191ce25a72160d2fbf0
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-06-29 10:01:45 -0700

    feat: allow batch.nc to take the year as an argument

--- tools/batch/batch_nc.sh
+++ tools/batch/batch_nc.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+# Usage ./batch_nc <year>
+
 find ~/dap.ceda.ac.uk/neodc/esacci/lakes/data/lake_products/L3S/v3.0/merged_product -type f -name "*.nc" -exec sh -c '
     name=$(basename "${1}" .nc)
     
     python main.py \
-        "$1" \
+        "${1}" \
         ~/dap.ceda.ac.uk/neodc/esacci/lakes/data/lake_products/L3S/v3.0/ESA_CCI_static_lake_mask.nc \
         ~/Downloads/WFSEL/ESA_CCI/data/csv/lakescci_v2.1.0_metadata_filtered.csv \
-        ~/Downloads/WFSEL/ESA_CCI/data/out/2023/${name}.csv
+        ~/Downloads/WFSEL/ESA_CCI/data/out/${1}/${name}.csv
     ' _ {} \;
