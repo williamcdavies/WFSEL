5176ca06982a2a2c032560dd7a16adeb4163c910
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-06-29 14:48:07 -0700

    fix: incorrect use of  in -exec block

--- tools/sh/batch_nc.sh
+++ tools/sh/batch_nc.sh
@@ -2,12 +2,12 @@
 
 # Usage ./batch_nc <year>
 
-find ~/dap.ceda.ac.uk/neodc/esacci/lakes/data/lake_products/L3S/v3.0/merged_product -type f -name "*.nc" -exec sh -c '
+find ~/dap.ceda.ac.uk/neodc/esacci/lakes/data/lake_products/L3S/v3.0/merged_product/${1} -type f -name "*.nc" -exec sh -c '
     name=$(basename "${1}" .nc)
     
     python main.py \
         "${1}" \
         ~/dap.ceda.ac.uk/neodc/esacci/lakes/data/lake_products/L3S/v3.0/ESA_CCI_static_lake_mask.nc \
         ~/Downloads/WFSEL/ESA_CCI/data/csv/lakescci_v2.1.0_metadata_filtered.csv \
-        ~/Downloads/WFSEL/ESA_CCI/data/out/${1}/${name}.csv
-    ' _ {} \;
+        ~/Downloads/WFSEL/ESA_CCI/data/out/${2}/${name}.csv
+    ' _ {} ${1} \;
