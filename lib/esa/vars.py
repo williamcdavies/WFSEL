62f98f48378734ad49532295a2ca84064374e000
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-07-14 12:28:36 -0700

    feat: define RETURN_SUCCESS and RETURN_FAILURE in dedicated lib.io.vars file, introduce lib.esa module, and create utils.py and vars.py files for each module

---
+++ lib/esa/vars.py
@@ -0,0 +1,8 @@
+ECVS = ['chla', 
+        'tsm', 
+        'acdom440', 
+        'Kd490', 
+        'KdPAR',
+        'phycocyanin', 
+        'lake_surface_water_temperature',
+        'lake_surface_water_extent']
