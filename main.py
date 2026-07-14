73904ea94b5b7525607f0e78e1b1fd3d9cc9f65c
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-07-14 12:32:02 -0700

    chore: modify whitespace

--- main.py
+++ main.py
@@ -170,4 +170,4 @@ def main() -> int:
 
 
 if __name__ == '__main__':
-    sys.exit(main())
+   sys.exit(main())
--- main_centroid.py
+++ main_centroid.py
@@ -177,4 +177,4 @@ def main() -> int:
 
 
 if __name__ == '__main__':
-    sys.exit(main())
+   sys.exit(main())
