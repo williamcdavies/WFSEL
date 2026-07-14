ef7a985cdb57e1f61d9f5d3f41b5001f23ef86c0
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-07-14 12:48:33 -0700

    split batch_main into batch_main and batch_main_centroid

--- tools/sh/batch_main.sh
+++ tools/sh/batch_main.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-# Usage ./batch_main.sh <year> <main*.py>
+# Usage ./batch_main.sh <year>
 
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 <year> <main*.py>"
+if [ $# -ne 1 ]; then
+    echo "Usage: ${0} <year>"
     
     exit 1
 fi
 
 mkdir -p ~/Downloads/WFSEL/ESA/data/lakes/out/"${1}"
 
-find ~/dap.ceda.ac.uk/neodc/esacci/lakes/data/lake_products/L3S/v3.0/merged_product/${1} -type f -name "*.nc" -exec sh -c '
-    year="$1"
-    script="$2"
-    ncfile="$3"
-    name=$(basename "$ncfile" .nc)
-    python "$script" \
-        "$ncfile" \
+find ~/dap.ceda.ac.uk/neodc/esacci/lakes/data/lake_products/L3S/v3.0/merged_product/"${1}" -type f -name "*.nc" -exec sh -c '
+    year="${1}"
+    ncfilename="${2}"
+    ncbasename=$(basename "${ncfilename}" .nc)
+
+    python main.py \
+        "$ncfilename" \
         ~/dap.ceda.ac.uk/neodc/esacci/lakes/data/lake_products/L3S/v3.0/ESA_CCI_static_lake_mask.nc \
         ~/Downloads/WFSEL/ESA/data/lakes/lakescci_v2.1.0_metadata_filtered.csv \
-        ~/Downloads/WFSEL/ESA/data/lakes/out/"${year}"/"${name}".csv
-    ' _ "${1}" "${2}" {} \;
+        ~/Downloads/WFSEL/ESA/data/lakes/out/"${year}"/"${ncbasename}".csv
+    ' _ "${1}" {} \;
---
+++ tools/sh/batch_main_centroid.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Usage ./batch_main_centroid.sh <buffer> <year>
+
+if [ $# -ne 2 ]; then
+    echo "Usage: ${0} <buffer> <year>"
+    
+    exit 1
+fi
+
+mkdir -p ~/Downloads/WFSEL/ESA/data/lakes/out/"${2}"
+
+find ~/dap.ceda.ac.uk/neodc/esacci/lakes/data/lake_products/L3S/v3.0/merged_product/"${2}" -type f -name "*.nc" -exec sh -c '
+    buffer="${1}"
+    year="${2}"
+    ncfilename="${3}"
+    ncbasename=$(basename "${ncfilename}" .nc)
+
+    python main_centroid.py \
+        "${ncfilename}" \
+        ~/Downloads/WFSEL/ESA/data/lakes/lakescci_v2.1.0_metadata_filtered.csv \
+        ~/Downloads/WFSEL/ESA/data/lakes/out/"${year}"/"${ncbasename}".csv \
+        "${buffer}"
+    ' _ "${1}" "${2}" {} \;
