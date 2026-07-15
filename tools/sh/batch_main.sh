2c759cc0edec53342b79bb581ed15a830af475a1
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-07-15 14:52:20 -0700

    feat: add argument checks to batch_main.sh

--- tools/sh/batch_main.sh
+++ tools/sh/batch_main.sh
@@ -1,25 +1,70 @@
 #!/bin/bash
 
-# Usage ./batch_main_centroid.sh <buffer> <year>
+# Usage ./batch_main.sh <buffer> <year>
 
-if [ $# -ne 2 ]; then
-    echo "Usage: ${0} <buffer> <year>"
+function help() {
+    echo "usage: ${0} [options]
+    
+Runs main.py on each netcdf file in ~/dap.ceda.ac.uk/neodc/esacci/lakes/data/lake_products/L3S/v3.0/merged_product/<year>
+
+positional arguments:
+    buffer
+        n in N | n >= 0 or 'inf'
+
+    year
+        s in ['2011', '2023']
+    "
+}
+
+# If first argument is `-h` or `--help`, or argument count is not 2, exit 1
+if [[ "${1}" = "-h" || "${1}" = "--help" || "$#" -ne 2 ]]; then
+    help
     
     exit 1
 fi
 
-mkdir -p ~/Downloads/WFSEL/ESA/data/lakes/out/"${2}"
+# Read first argument into `buffer`
+buffer="${1}"
 
-find ~/dap.ceda.ac.uk/neodc/esacci/lakes/data/lake_products/L3S/v3.0/merged_product/"${2}" -type f -name "*.nc" -exec sh -c '
+# Read second argument into `year`
+year="${2}"
+
+# If `buffer` is invalid, exit 1
+if [[ ! "${buffer}" =~ ^([0-9]+|inf)$ ]]; then
+    echo "error: argument buffer: invalid int value: "${buffer}""
+
+    exit 1
+fi
+
+# If `year` is invalid, exit 1
+if [[ ! "${year}" =~ ^(2011|2023)$ ]]; then
+    echo "error: argument year: invalid int value: "${year}""
+
+    exit 1
+fi
+
+# Ensure output directory exists
+mkdir -p ~/Downloads/WFSEL/ESA/lakes/data/"${year}"
+
+# Run `main.py` on each netcdf file in ~/dap.ceda.ac.uk/neodc/esacci/lakes/data/lake_products/L3S/v3.0/merged_product/<year>
+find ~/dap.ceda.ac.uk/neodc/esacci/lakes/data/lake_products/L3S/v3.0/merged_product/"${year}" -type f -name "*.nc" -exec sh -c '
+    # Read first argument into `buffer`
     buffer="${1}"
+    
+    # Read second argument into `year`
     year="${2}"
+
+    # Read third argument into `ncfilename`
     ncfilename="${3}"
+
+    # Read `ncfilename` basename into `ncbasename`
     ncbasename=$(basename "${ncfilename}" .nc)
 
-    python main_centroid.py \
+    # Run `main.py`
+    python main.py \
         "${buffer}" \
         "${ncfilename}" \
         ~/dap.ceda.ac.uk/neodc/esacci/lakes/data/lake_products/L3S/v3.0/ESA_CCI_static_lake_mask.nc \
-        ~/Downloads/WFSEL/ESA/data/lakes/lakescci_v2.1.0_metadata_filtered.csv \
-        ~/Downloads/WFSEL/ESA/data/lakes/out/"${year}"/"${ncbasename}".csv
-    ' _ "${1}" "${2}" {} \;
+        ~/Downloads/WFSEL/ESA/lakes/data/lakescci_v2.1.0_metadata_filtered.csv \
+        ~/Downloads/WFSEL/ESA/lakes/data/"${year}"/"${ncbasename}".csv
+    ' _ "${buffer}" "${year}" {} \;
