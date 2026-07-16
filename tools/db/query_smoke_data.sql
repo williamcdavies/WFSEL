6ece9855a28e6d6271787896fcc385ae64476c04
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-07-16 14:48:12 -0700

    feat: variableize keywords

--- tools/db/query_smoke_data.sql
+++ tools/db/query_smoke_data.sql
@@ -14,13 +14,13 @@ COPY (
         FROM lakes       AS l
         JOIN lakes_polys AS lp
         ON l.id = lp.id
-        WHERE l.id = 0
+        WHERE l.id = :lakes_id
     )
     SELECT
         x.name      AS "name",
         s.start_day AS "day"
-    FROM hms_smokes2023 AS s
-    JOIN x              AS x
+    FROM :hms_smokes_table AS s
+    JOIN x                 AS x
     ON ST_INTERSECTS(s.geom, x.geom)
     WHERE s.density > 1
     ORDER BY s.start_day
