255f5d65a3db690d351c05c7036f90506d4f9923
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-07-30 10:26:02 -0700

    build: add psycopg and pygam to dependencies

---
+++ lib/lakes_cci/utils.py
@@ -0,0 +1,8 @@
+r'''
+utils.py
+
+Description: 
+   Provide definitions for lakes_cci-utility functions.
+
+Written by William Chuter-Davies
+'''
---
+++ lib/lakes_cci/vars.py
@@ -0,0 +1,25 @@
+r'''
+utils.py
+
+Description: 
+   Provide definitions for lakes_cci-utility variables.
+
+Written by William Chuter-Davies
+'''
+
+
+ECVS           = ['chla', 
+                  'tsm', 
+                  'acdom440', 
+                  'Kd490', 
+                  'KdPAR', 
+                  'phycocyanin', 
+                  'lake_surface_water_temperature', 
+                  'lake_surface_water_extent']
+MEASURES       = ['mean', 
+                  'median', 
+                  'var', 
+                  'max', 
+                  'min']
+LOWER_QUARTILE = 6
+UPPER_QUARTILE = 27
