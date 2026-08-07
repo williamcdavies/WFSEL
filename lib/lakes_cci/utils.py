fb7bed5dc9b081b83ef70ba87c0a6795352d48c0
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-08-07 15:25:47 -0700

    refactor: extract stampable argparse blocks and rename certain variables to better match usage

--- lib/lakes_cci/utils.py
+++ lib/lakes_cci/utils.py
@@ -13,6 +13,39 @@ import argparse
 import pathlib
 
 
+def add_argument_lakes_cci_count_of_smoke_days_csv_path(parser: argparse.ArgumentParser) -> None:
+   '''
+   Adds a `lakes_cci_count_of_smoke_days_csv_path` argument to a
+   :class:`argparse.ArgumentParser`.
+
+   Parameters
+   ----------
+   parser : :class:`argparse.ArgumentParser`
+      The parser
+   '''
+   parser.add_argument('lakes_cci_count_of_smoke_days_csv_path', 
+                       type=pathlib.Path, 
+                       help=f'''path to some count of smoke days data
+                             csv file as produced by
+                             query_lakes_cci_count_of_smoke_days.sql''')
+
+
+def add_argument_lakes_cci_ecv_data_dir_path(parser: argparse.ArgumentParser) -> None:
+   '''
+   Adds a `lakes_cci_ecv_data_dir_path` argument to a
+   :class:`argparse.ArgumentParser`.
+
+   Parameters
+   ----------
+   parser : :class:`argparse.ArgumentParser`
+      The parser
+   '''
+   parser.add_argument('lakes_cci_ecv_data_dir_path', 
+                       type=pathlib.Path, 
+                       help=f'''path to the Lakes ECV data directory as
+                             produced by main.py''')
+
+
 def add_argument_lakes_cci_merged_prod_nc_path(parser: argparse.ArgumentParser) -> None:
    '''
    Adds a `lakes_cci_merged_prod_nc_path` argument to a
@@ -20,12 +53,8 @@ def add_argument_lakes_cci_merged_prod_nc_path(parser: argparse.ArgumentParser) 
 
    Parameters
    ----------
-   parse : :class:`argparse.ArgumentParser`
+   parser : :class:`argparse.ArgumentParser`
       The parser
-
-   Notes
-   -----
-   Type of `args.lakes_cci_merged_prod_nc_path` is :class:`pathlib.Path`
    '''
    parser.add_argument('lakes_cci_merged_prod_nc_path', 
                        type=pathlib.Path, 
@@ -43,12 +72,8 @@ def add_argument_lakes_cci_meta_data_csv_path(parser: argparse.ArgumentParser) -
 
    Parameters
    ----------
-   parse : :class:`argparse.ArgumentParser`
+   parser : :class:`argparse.ArgumentParser`
       The parser
-
-   Notes
-   -----
-   Type of `args.lakes_cci_meta_data_csv_path` is :class:`pathlib.Path`
    '''
    parser.add_argument('lakes_cci_meta_data_csv_path',
                        type=pathlib.Path,
@@ -66,12 +91,8 @@ def add_argument_lakes_cci_static_mask_nc_path(parser: argparse.ArgumentParser) 
 
    Parameters
    ----------
-   parse : :class:`argparse.ArgumentParser`
+   parser : :class:`argparse.ArgumentParser`
       The parser
-
-   Notes
-   -----
-   Type of `args.lakes_cci_static_mask_nc_path` is :class:`pathlib.Path`
    '''
    parser.add_argument('lakes_cci_static_mask_nc_path', 
                        type=pathlib.Path, 
@@ -80,6 +101,57 @@ def add_argument_lakes_cci_static_mask_nc_path(parser: argparse.ArgumentParser) 
                              provided by ESA Lakes Climate Change
                              Initiative (Lakes_cci): Lake products,
                              Version 3.0''')
+
+
+def argument_lakes_cci_count_of_smoke_days_csv_path_exists(lakes_cci_count_of_smoke_days_csv_path: pathlib.Path, 
+                                                           *, 
+                                                           loud=True) -> bool:
+   '''
+   Returns true if `lakes_cci_count_of_smoke_days_csv_path` exists,
+   returns false otherwise.
+
+   Parameters
+   ----------
+   lakes_cci_count_of_smoke_days_csv_path : :class:`pathlib.Path`
+      The argument `lakes_cci_count_of_smoke_days_csv_path`
+
+   loud : bool
+      If true, prints an error message to stdout. default=False
+   '''
+   if lakes_cci_count_of_smoke_days_csv_path.exists():
+      return True
+
+   if loud:
+      print(f'''error: argument lakes_cci_count_of_smoke_days_csv_path:
+             no such file or directory:
+             {lakes_cci_count_of_smoke_days_csv_path}''')
+
+   return False
+
+
+def argument_lakes_cci_ecv_data_dir_path_exists(lakes_cci_ecv_data_dir_path: pathlib.Path, 
+                                                *, 
+                                                loud=True) -> bool:
+   '''
+   Returns true if `lakes_cci_ecv_data_dir_path` exists, returns false
+   otherwise.
+
+   Parameters
+   ----------
+   lakes_cci_ecv_data_dir_path : :class:`pathlib.Path`
+      The argument `lakes_cci_ecv_data_dir_path`
+
+   loud : bool
+      If true, prints an error message to stdout. default=False
+   '''
+   if lakes_cci_ecv_data_dir_path.exists():
+      return True
+
+   if loud:
+      print(f'''error: argument lakes_cci_ecv_data_dir_path: no such
+             file or directory: {lakes_cci_ecv_data_dir_path}''')
+
+   return False
 
 
 def argument_lakes_cci_merged_prod_nc_path_exists(lakes_cci_merged_prod_nc_path: pathlib.Path, 
