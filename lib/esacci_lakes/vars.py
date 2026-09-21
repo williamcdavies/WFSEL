9fd9ec50067a4b8e7699fccdf32540e5356ffc53
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-09-21 11:01:40 -0700

    chore: add definitions for ESA CCI cover classes

--- lib/esacci_lakes/vars.py
+++ lib/esacci_lakes/vars.py
@@ -16,43 +16,55 @@ from lib.esacci_lakes.objects import (
 COUNT_OF_DISTINCT_START_DAYS_LOWER_BOUND = 7
 COUNT_OF_DISTINCT_START_DAYS_UPPER_BOUND = 42
 
+ESACCI_LAKES_COVER_CLASS_WATER = 1
+ESACCI_LAKES_COVER_CLASS_ICE   = 2
+ESACCI_LAKES_COVER_CLASS_CLOUD = 3
+
 ESACCI_LAKES_VARIABLES = {
-    "chla": ESACCILakesVariable(
+    "chla":
+    ESACCILakesVariable(
         "chla",
         "Concentration of Chlorophyll-a",
         "mg.m3"
     ),
-    "tsm": ESACCILakesVariable(
+    "tsm":
+    ESACCILakesVariable(
         "tsm",
         "Concentration of Total Suspended Matter",
         "g.m3"
     ),
-    "acdom440": ESACCILakesVariable(
+    "acdom440":
+    ESACCILakesVariable(
         "acdom440",
         "Absorption Coefficient of Coloured Dissolved Organic Matter at 440 nm",
         "m"
     ),
-    "Kd490": ESACCILakesVariable(
+    "Kd490":
+    ESACCILakesVariable(
         "Kd490",
         "Vertical Diffuse Downwelling Attenuation Coefficient at 490 nm",
         "m"
     ),
-    "KdPAR": ESACCILakesVariable(
+    "KdPAR":
+    ESACCILakesVariable(
         "KdPAR",
         "Vertical Diffuse Downwelling Attenuation Coefficient Aggregated Over PAR",
         "m"
     ),
-    "phycocyanin": ESACCILakesVariable(
+    "phycocyanin":
+    ESACCILakesVariable(
         "phycocyanin",
         "Concentration of Phycocyanin Calculated From MDN Algorithm by O'Shea et al. 2021",
         "mg.m3"
     ),
-    "lake_surface_water_temperature": ESACCILakesVariable(
+    "lake_surface_water_temperature":
+    ESACCILakesVariable(
         "lake_surface_water_temperature",
         "Lake Surface Skin Temperature",
         "˚C"
     ),
-    "lake_surface_water_extent": ESACCILakesVariable(
+    "lake_surface_water_extent":
+    ESACCILakesVariable(
         "lake_surface_water_extent",
         "Lake Water Extent",
         "km2"
@@ -60,7 +72,8 @@ ESACCI_LAKES_VARIABLES = {
 }
 
 HYLAK_FIELDS = {
-    "hylak_id": HylakField(
+    "hylak_id":
+    HylakField(
         "hylak_id",
         "HydroLAKES ID",
         "",
@@ -70,7 +83,8 @@ HYLAK_FIELDS = {
         None,
         None
     ),
-    "lake_name": HylakField(
+    "lake_name":
+    HylakField(
         "lake_name",
         "Lake Name",
         "",
@@ -80,7 +94,8 @@ HYLAK_FIELDS = {
         None,
         None
     ),
-    "country": HylakField(
+    "country":
+    HylakField(
         "country",
         "Country",
         "",
@@ -90,7 +105,8 @@ HYLAK_FIELDS = {
         None,
         None
     ),
-    "continent": HylakField(
+    "continent":
+    HylakField(
         "continent",
         "Continent",
         "",
@@ -100,7 +116,8 @@ HYLAK_FIELDS = {
         None,
         None
     ),
-    "poly_src": HylakField(
+    "poly_src":
+    HylakField(
         "poly_src",
         "Polygon Source",
         "",
@@ -110,7 +127,8 @@ HYLAK_FIELDS = {
         None,
         None
     ),
-    "lake_type": HylakField(
+    "lake_type":
+    HylakField(
         "lake_type",
         "Lake Type",
         "",
@@ -120,7 +138,8 @@ HYLAK_FIELDS = {
         None,
         None
     ),
-    "grand_id": HylakField(
+    "grand_id":
+    HylakField(
         "grand_id",
         "GRanD Reservoir ID",
         "",
@@ -130,7 +149,8 @@ HYLAK_FIELDS = {
         None,
         None
     ),
-    "lake_area_m2": HylakField(
+    "lake_area_m2":
+    HylakField(
         "lake_area_m2",
         "Lake Area",
         "m2",
@@ -140,7 +160,8 @@ HYLAK_FIELDS = {
         100e6,
         500e6
     ),
-    "shore_len_m": HylakField(
+    "shore_len_m":
+    HylakField(
         "shore_len_m",
         "Shoreline Length",
         "m1",
@@ -150,7 +171,8 @@ HYLAK_FIELDS = {
         None,
         None
     ),
-    "shore_dev": HylakField(
+    "shore_dev":
+    HylakField(
         "shore_dev",
         "Shoreline Development",
         "",
@@ -160,7 +182,8 @@ HYLAK_FIELDS = {
         None,
         None
     ),
-    "vol_total_m3": HylakField(
+    "vol_total_m3":
+    HylakField(
         "vol_total_m3",
         "Total Lake Volume",
         "m3",
@@ -170,7 +193,8 @@ HYLAK_FIELDS = {
         0.5e9,
         5e9
     ),
-    "vol_res_m3": HylakField(
+    "vol_res_m3":
+    HylakField(
         "vol_res_m3",
         "Reservoir Volume",
         "m3",
@@ -180,7 +204,8 @@ HYLAK_FIELDS = {
         None,
         None
     ),
-    "vol_src": HylakField(
+    "vol_src":
+    HylakField(
         "vol_src",
         "Volume Source",
         "",
@@ -190,7 +215,8 @@ HYLAK_FIELDS = {
         None,
         None
     ),
-    "depth_avg_m": HylakField(
+    "depth_avg_m":
+    HylakField(
         "depth_avg_m",
         "Average Depth",
         "m",
@@ -200,7 +226,8 @@ HYLAK_FIELDS = {
         5,
         20
     ),
-    "dis_avg_m3_per_s": HylakField(
+    "dis_avg_m3_per_s":
+    HylakField(
         "dis_avg_m3_per_s",
         "Average Discharge",
         "m3.s",
@@ -210,7 +237,8 @@ HYLAK_FIELDS = {
         None,
         None
     ),
-    "res_time_days": HylakField(
+    "res_time_days":
+    HylakField(
         "res_time_days",
         "Residence Time",
         "days",
@@ -220,7 +248,8 @@ HYLAK_FIELDS = {
         None,
         None
     ),
-    "elevation_m": HylakField(
+    "elevation_m":
+    HylakField(
         "elevation_m",
         "Elevation",
         "m",
@@ -230,7 +259,8 @@ HYLAK_FIELDS = {
         100,
         500
     ),
-    "slope_100_m_per_km": HylakField(
+    "slope_100_m_per_km":
+    HylakField(
         "slope_100_m_per_km",
         "Shoreline Slope",
         "m.km",
@@ -240,7 +270,8 @@ HYLAK_FIELDS = {
         None,
         None
     ),
-    "wshd_area_m2": HylakField(
+    "wshd_area_m2":
+    HylakField(
         "wshd_area_m2",
         "Watershed Area",
         "m2",
@@ -250,7 +281,8 @@ HYLAK_FIELDS = {
         None,
         None
     ),
-    "pour_long": HylakField(
+    "pour_long":
+    HylakField(
         "pour_long",
         "Pour Point Longitude",
         "degrees",
@@ -260,7 +292,8 @@ HYLAK_FIELDS = {
         None,
         None
     ),
-    "pour_lat": HylakField(
+    "pour_lat":
+    HylakField(
         "pour_lat",
         "Pour Point Latitude",
         "degrees",
