7e5b66ec945a1d45555abb34a82a3e37ecd2f3f5
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-09-21 12:22:31 -0700

    feat: define get_esacci_lakes_merged_product_nc_paths()

--- lib/esacci_lakes/utils/io.py
+++ lib/esacci_lakes/utils/io.py
@@ -138,6 +138,34 @@ def add_argument_esacci_lakes_counts_of_distinct_start_days_csv_path(
     )
 
 
+def add_argument_esacci_lakes_hylak_fields_csv_path(
+    parser: argparse.ArgumentParser
+) -> None:
+    """
+    Adds a `esacci_lakes_hylak_fields_csv_path` argument to a
+    :class:`ArgumentParser`.
+
+    Parameters
+    ----------
+    parser : :class:`ArgumentParser`
+        The parser
+
+    Returns
+    -------
+    None
+
+    Notes
+    -----
+    Argument `esacci_lakes_hylak_fields_csv_path` is of type
+    :class:`Path`.
+    """
+    parser.add_argument(
+        "esacci_lakes_hylak_fields_csv_path",
+        type = Path,
+        help = """path to some hylak fields data csv file as produced by query_esacci_lakes_for_hylak_fields.sql"""
+    )
+
+
 def argument_esacci_lakes_counts_of_distinct_start_days_csv_path_exists(
     esacci_lakes_counts_of_distinct_start_days_csv_path: Path,
     *,
@@ -167,64 +195,6 @@ def argument_esacci_lakes_counts_of_distinct_start_days_csv_path_exists(
         print(f"""error: argument esacci_lakes_counts_of_distinct_start_days_csv_path: no such file or directory: {esacci_lakes_counts_of_distinct_start_days_csv_path}""")
 
     return False
-
-
-def read_esacci_lakes_counts_of_distinct_start_days_csv(
-    esacci_lakes_counts_of_distinct_start_days_csv_path: Path
-) -> pd.DataFrame:
-    """
-    Reads `esacci_lakes_counts_of_distinct_start_days_csv_path` into a
-    :class:`pandas.DataFrame`.
-
-    Parameters
-    ----------
-    esacci_lakes_counts_of_distinct_start_days_csv_path : :class:`pathlib.Path`
-        The path to some counts of distinct start days data csv file as
-        produced by
-        query_esacci_lakes_for_counts_of_distinct_start_days.sql
-
-    Returns
-    -------
-    A :class:`pandas.DataFrame`.
-
-    Notes
-    -----
-    Internal `pandas.read_csv` call assumes "esacci_lakes_id" is an
-    existing column in the csv file at
-    `esacci_lakes_counts_of_distinct_start_days_csv_path`.
-    """
-    return pd.read_csv(
-        esacci_lakes_counts_of_distinct_start_days_csv_path,
-        index_col = "esacci_lakes_id"
-    )
-
-
-def add_argument_esacci_lakes_hylak_fields_csv_path(
-    parser: argparse.ArgumentParser
-) -> None:
-    """
-    Adds a `esacci_lakes_hylak_fields_csv_path` argument to a
-    :class:`ArgumentParser`.
-
-    Parameters
-    ----------
-    parser : :class:`ArgumentParser`
-        The parser
-
-    Returns
-    -------
-    None
-
-    Notes
-    -----
-    Argument `esacci_lakes_hylak_fields_csv_path` is of type
-    :class:`Path`.
-    """
-    parser.add_argument(
-        "esacci_lakes_hylak_fields_csv_path",
-        type = Path,
-        help = """path to some hylak fields data csv file as produced by query_esacci_lakes_for_hylak_fields.sql"""
-    )
 
 
 def argument_esacci_lakes_hylak_fields_csv_path_exists(
@@ -282,6 +252,36 @@ def read_esacci_lakes_hylak_fields_csv(
     """
     return pd.read_csv(
         esacci_lakes_hylak_fields_csv_path,
+        index_col = "esacci_lakes_id"
+    )
+
+
+def read_esacci_lakes_counts_of_distinct_start_days_csv(
+    esacci_lakes_counts_of_distinct_start_days_csv_path: Path
+) -> pd.DataFrame:
+    """
+    Reads `esacci_lakes_counts_of_distinct_start_days_csv_path` into a
+    :class:`pandas.DataFrame`.
+
+    Parameters
+    ----------
+    esacci_lakes_counts_of_distinct_start_days_csv_path : :class:`pathlib.Path`
+        The path to some counts of distinct start days data csv file as
+        produced by
+        query_esacci_lakes_for_counts_of_distinct_start_days.sql
+
+    Returns
+    -------
+    A :class:`pandas.DataFrame`.
+
+    Notes
+    -----
+    Internal `pandas.read_csv` call assumes "esacci_lakes_id" is an
+    existing column in the csv file at
+    `esacci_lakes_counts_of_distinct_start_days_csv_path`.
+    """
+    return pd.read_csv(
+        esacci_lakes_counts_of_distinct_start_days_csv_path,
         index_col = "esacci_lakes_id"
     )
 
@@ -404,6 +404,117 @@ def add_argument_esacci_lakes_variable(
     )
 
 
+def add_argument_esacci_lakes_metadata_csv_path(
+    parser: argparse.ArgumentParser
+) -> None:
+    """
+    Adds a `esacci_lakes_metadata_csv_path` argument to a
+    :class:`ArgumentParser`.
+
+    Parameters
+    ----------
+    parser : :class:`ArgumentParser`
+        The parser
+
+    Returns
+    -------
+    None
+
+    Notes
+    -----
+    Argument `esacci_lakes_metadata_csv_path` is of type :class:`Path`.
+    """
+    parser.add_argument(
+        "esacci_lakes_metadata_csv_path",
+        type = Path,
+        help = """path to the `lakescci_v2.1.0_metadata.csv` file as provided by ESA Lakes Climate Change Initiative (Lakes_cci): Lake products, Version 3.0"""
+    )
+
+
+def add_argument_esacci_lakes_static_lake_mask_nc_path(
+    parser: argparse.ArgumentParser
+) -> None:
+    """
+    Adds a `esacci_lakes_static_lake_mask_nc_path` argument to a
+    :class:`ArgumentParser`.
+
+    Parameters
+    ----------
+    parser : :class:`ArgumentParser`
+        The parser
+
+    Returns
+    -------
+    None
+
+    Notes
+    -----
+    Argument `esacci_lakes_static_lake_mask_nc_path` is of type
+    :class:`Path`.
+    """
+    parser.add_argument(
+        "esacci_lakes_static_lake_mask_nc_path",
+        type = Path,
+        help = """path to the `ESA_CCI_static_lake_mask.nc` file as provided by ESA Lakes Climate Change Initiative (Lakes_cci): Lake products, Version 3.0"""
+    )
+
+
+def add_argument_esacci_lakes_merged_product_dir_path(
+    parser: argparse.ArgumentParser
+) -> None:
+    """
+    Adds a `esacci_lakes_merged_product_dir_path` argument to a
+    :class:`ArgumentParser`.
+
+    Parameters
+    ----------
+    parser : :class:`ArgumentParser`
+        The parser
+
+    Returns
+    -------
+    None
+
+    Notes
+    -----
+    Argument `esacci_lakes_merged_product_dir_path` is of type
+    :class:`Path`.
+    """
+    parser.add_argument(
+        "esacci_lakes_merged_product_dir_path",
+        type = Path,
+        help = """path to the ESA CCI merged_product directory as provided by ESA Lakes Climate Change Initiative (Lakes_cci): Lake products, Version 3.0"""
+    )
+
+
+def add_argument_esacci_lakes_merged_product_nc_path(
+    parser: argparse.ArgumentParser
+) -> None:
+    """
+    Adds a `esacci_lakes_merged_product_nc_path` argument to a
+    :class:`ArgumentParser`.
+
+    Parameters
+    ----------
+    parser : :class:`ArgumentParser`
+        The parser
+
+    Returns
+    -------
+    None
+
+    Notes
+    -----
+    Argument `esacci_lakes_merged_product_nc_path` is of type
+    :class:`Path`.
+    """
+    parser.add_argument(
+        "esacci_lakes_merged_product_nc_path",
+        type = Path,
+        help = """path to some `ESACCI-LAKES-L3S-LK_PRODUCTS-MERGED-YYYYMMDD-fv3.0.0.nc` file as provided by ESA Lakes Climate Change Initiative (Lakes_cci): Lake products, Version 3.0"""
+    )
+
+
 def argument_esacci_lakes_variable_is_in_esacci_lakes_variables(
     esacci_lakes_variable: str,
     *,
@@ -434,33 +545,6 @@ def argument_esacci_lakes_variable_is_in_esacci_lakes_variables(
     return False
 
 
-def add_argument_esacci_lakes_metadata_csv_path(
-    parser: argparse.ArgumentParser
-) -> None:
-    """
-    Adds a `esacci_lakes_metadata_csv_path` argument to a
-    :class:`ArgumentParser`.
-
-    Parameters
-    ----------
-    parser : :class:`ArgumentParser`
-        The parser
-
-    Returns
-    -------
-    None
-
-    Notes
-    -----
-    Argument `esacci_lakes_metadata_csv_path` is of type :class:`Path`.
-    """
-    parser.add_argument(
-        "esacci_lakes_metadata_csv_path",
-        type = Path,
-        help = """path to the `lakescci_v2.1.0_metadata.csv` file as provided by ESA Lakes Climate Change Initiative (Lakes_cci): Lake products, Version 3.0"""
-    )
-
-
 def argument_esacci_lakes_metadata_csv_path_exists(
     esacci_lakes_metadata_csv_path: Path,
     *,
@@ -487,6 +571,96 @@ def argument_esacci_lakes_metadata_csv_path_exists(
 
     if loud:
         print(f"""error: argument esacci_lakes_metadata_csv_path: no such file or directory: {esacci_lakes_metadata_csv_path}""")
+
+    return False
+
+
+def argument_esacci_lakes_static_lake_mask_nc_path_exists(
+    esacci_lakes_static_lake_mask_nc_path: Path,
+    *,
+    loud:                                  bool = False
+) -> bool:
+    """
+    Validates `esacci_lakes_static_lake_mask_nc_path`.
+
+    Parameters
+    ----------
+    esacci_lakes_static_lake_mask_nc_path : :class:`Path`
+        The argument `esacci_lakes_static_lake_mask_nc_path`
+
+    loud : bool
+        If `True`, prints an error message to stdout. default=False
+
+    Returns
+    -------
+    `True` if `esacci_lakes_static_lake_mask_nc_path` exists. `False`
+    otherwise.
+    """
+    if esacci_lakes_static_lake_mask_nc_path.exists():
+        return True
+
+    if loud:
+        print(f"""error: argument esacci_lakes_static_lake_mask_nc_path: no such file or directory: {esacci_lakes_static_lake_mask_nc_path}""")
+
+    return False
+
+
+def argument_esacci_lakes_merged_product_dir_path_exists(
+    esacci_lakes_merged_product_dir_path: Path,
+    *,
+    loud:                                 bool = False
+) -> bool:
+    """
+    Validates `esacci_lakes_merged_product_dir_path`.
+
+    Parameters
+    ----------
+    esacci_lakes_merged_product_dir_path : :class:`Path`
+        The argument `esacci_lakes_merged_product_dir_path`
+
+    loud : bool
+        If `True`, prints an error message to stdout. default=False
+
+    Returns
+    -------
+    `True` if `esacci_lakes_merged_product_dir_path` exists. `False`
+    otherwise.
+    """
+    if esacci_lakes_merged_product_dir_path.exists():
+        return True
+
+    if loud:
+        print(f"""error: argument esacci_lakes_merged_product_dir_path: no such file or directory: {esacci_lakes_merged_product_dir_path}""")
+
+    return False
+
+
+def argument_esacci_lakes_merged_product_nc_path_exists(
+    esacci_lakes_merged_product_nc_path: Path,
+    *,
+    loud:                                bool = False
+) -> bool:
+    """
+    Validates `esacci_lakes_merged_product_nc_path`.
+
+    Parameters
+    ----------
+    esacci_lakes_merged_product_nc_path : :class:`Path`
+        The argument `esacci_lakes_merged_product_nc_path`
+
+    loud : bool
+        If `True`, prints an error message to stdout. default=False
+
+    Returns
+    -------
+    `True` if `esacci_lakes_merged_product_nc_path` exists. `False`
+    otherwise.
+    """
+    if esacci_lakes_merged_product_nc_path.exists():
+        return True
+
+    if loud:
+        print(f"""error: argument esacci_lakes_merged_product_nc_path: no such file or directory: {esacci_lakes_merged_product_nc_path}""")
 
     return False
 
@@ -521,178 +695,23 @@ def read_esacci_lakes_metadata_csv(
     )
 
 
-def add_argument_esacci_lakes_static_lake_mask_nc_path(
-    parser: argparse.ArgumentParser
-) -> None:
-    """
-    Adds a `esacci_lakes_static_lake_mask_nc_path` argument to a
-    :class:`ArgumentParser`.
-
-    Parameters
-    ----------
-    parser : :class:`ArgumentParser`
-        The parser
-
-    Returns
-    -------
-    None
-
-    Notes
-    -----
-    Argument `esacci_lakes_static_lake_mask_nc_path` is of type
-    :class:`Path`.
-    """
-    parser.add_argument(
-        "esacci_lakes_static_lake_mask_nc_path",
-        type = Path,
-        help = """path to the `ESA_CCI_static_lake_mask.nc` file as provided by ESA Lakes Climate Change Initiative (Lakes_cci): Lake products, Version 3.0"""
-    )
-
-
-def argument_esacci_lakes_static_lake_mask_nc_path_exists(
-    esacci_lakes_static_lake_mask_nc_path: Path,
-    *,
-    loud:                                  bool = False
-) -> bool:
-    """
-    Validates `esacci_lakes_static_lake_mask_nc_path`.
-
-    Parameters
-    ----------
-    esacci_lakes_static_lake_mask_nc_path : :class:`Path`
-        The argument `esacci_lakes_static_lake_mask_nc_path`
-
-    loud : bool
-        If `True`, prints an error message to stdout. default=False
-
-    Returns
-    -------
-    `True` if `esacci_lakes_static_lake_mask_nc_path` exists. `False`
-    otherwise.
-    """
-    if esacci_lakes_static_lake_mask_nc_path.exists():
-        return True
-
-    if loud:
-        print(f"""error: argument esacci_lakes_static_lake_mask_nc_path: no such file or directory: {esacci_lakes_static_lake_mask_nc_path}""")
-
-    return False
-
-
-def add_argument_esacci_lakes_merged_product_dir_path(
-    parser: argparse.ArgumentParser
-) -> None:
-    """
-    Adds a `esacci_lakes_merged_product_dir_path` argument to a
-    :class:`ArgumentParser`.
-
-    Parameters
-    ----------
-    parser : :class:`ArgumentParser`
-        The parser
-
-    Returns
-    -------
-    None
-
-    Notes
-    -----
-    Argument `esacci_lakes_merged_product_dir_path` is of type
-    :class:`Path`.
-    """
-    parser.add_argument(
-        "esacci_lakes_merged_product_dir_path",
-        type = Path,
-        help = """path to the ESA CCI merged_product directory as provided by ESA Lakes Climate Change Initiative (Lakes_cci): Lake products, Version 3.0"""
-    )
-
-
-def argument_esacci_lakes_merged_product_dir_path_exists(
+def get_esacci_lakes_merged_product_nc_paths(
     esacci_lakes_merged_product_dir_path: Path,
-    *,
-    loud:                                 bool = False
-) -> bool:
+) -> list[Path]:
     """
-    Validates `esacci_lakes_merged_product_dir_path`.
+    Returns a list of all ESA CCI Lakes merged product netCDF file paths
+    in `esacci_lakes_merged_product_dir_path`.
 
     Parameters
     ----------
-    esacci_lakes_merged_product_dir_path : :class:`Path`
-        The argument `esacci_lakes_merged_product_dir_path`
-
-    loud : bool
-        If `True`, prints an error message to stdout. default=False
+    esacci_lakes_merged_product_dir_path : :class:`pathlib.Path`
+        The directory
 
     Returns
     -------
-    `True` if `esacci_lakes_merged_product_dir_path` exists. `False`
-    otherwise.
+    A list of :class:`pathlib.Path`.
     """
-    if esacci_lakes_merged_product_dir_path.exists():
-        return True
-
-    if loud:
-        print(f"""error: argument esacci_lakes_merged_product_dir_path: no such file or directory: {esacci_lakes_merged_product_dir_path}""")
-
-    return False
-
-
-def add_argument_esacci_lakes_merged_product_nc_path(
-    parser: argparse.ArgumentParser
-) -> None:
-    """
-    Adds a `esacci_lakes_merged_product_nc_path` argument to a
-    :class:`ArgumentParser`.
-
-    Parameters
-    ----------
-    parser : :class:`ArgumentParser`
-        The parser
-
-    Returns
-    -------
-    None
-
-    Notes
-    -----
-    Argument `esacci_lakes_merged_product_nc_path` is of type
-    :class:`Path`.
-    """
-    parser.add_argument(
-        "esacci_lakes_merged_product_nc_path",
-        type = Path,
-        help = """path to some `ESACCI-LAKES-L3S-LK_PRODUCTS-MERGED-YYYYMMDD-fv3.0.0.nc` file as provided by ESA Lakes Climate Change Initiative (Lakes_cci): Lake products, Version 3.0"""
-    )
-
-
-def argument_esacci_lakes_merged_product_nc_path_exists(
-    esacci_lakes_merged_product_nc_path: Path,
-    *,
-    loud:                                bool = False
-) -> bool:
-    """
-    Validates `esacci_lakes_merged_product_nc_path`.
-
-    Parameters
-    ----------
-    esacci_lakes_merged_product_nc_path : :class:`Path`
-        The argument `esacci_lakes_merged_product_nc_path`
-
-    loud : bool
-        If `True`, prints an error message to stdout. default=False
-
-    Returns
-    -------
-    `True` if `esacci_lakes_merged_product_nc_path` exists. `False`
-    otherwise.
-    """
-    if esacci_lakes_merged_product_nc_path.exists():
-        return True
-
-    if loud:
-        print(f"""error: argument esacci_lakes_merged_product_nc_path: no such file or directory: {esacci_lakes_merged_product_nc_path}""")
-
-    return False
+    return list(esacci_lakes_merged_product_dir_path.glob("**/*.nc"))
 
 
 # ==================================================================================================
