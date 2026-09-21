59b357d93f8360cab786bc25fe504446bfcd060d
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-09-21 11:17:30 -0700

    feat: add definition for get_logstream

--- lib/io/utils.py
+++ lib/io/utils.py
@@ -6,3 +6,35 @@ Description:
 
 Written by William Chuter-Davies
 """
+
+# Standard Library Imports
+from datetime import datetime
+from pathlib import  Path
+from typing  import  TextIO
+
+
+def get_logstream(
+   wdir: Path
+) -> TextIO:
+   """
+   Opens a log file for appending.
+
+   Parameters
+   ----------
+   wdir : :class:`pathlib.Path`
+      The write directory
+
+   Returns
+   -------
+   An open, writable file object.
+
+   Notes
+   -----
+   The returned file object is a context manager.
+   """
+   timestamp = datetime.now().strftime("%Y%m%dT%H%M%S")
+
+   return open(
+      wdir / (timestamp + ".log"),
+      "a"
+   )
